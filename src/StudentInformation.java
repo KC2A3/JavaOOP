692470b73f9a6b0fc8480f00eb4160fb17cc0824
KC2A3 <947496351@qq.com>
2019-02-15 01:46:53 +0800

    Print student information

---
+++ src/StudentInformation.java
@@ -0,0 +1,12 @@
+import model.Student;
+
+public class StudentInformation {
+  public static void main(String args) {
+    Student student = new Student();
+    student.setName("刘昱涵");
+    student.setAge(23);
+    student.setGender("man");
+    student.setID(666);
+    System.out.println(student);
+  }
+}
